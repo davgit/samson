c393ddab87a949cface72c316b35d076ba64aadd
Sergio Nunes <snunes@zendesk.com>
2015-09-16 11:29:24 +0100

    Added support for changing project level roles for Users (admin menu).
    Introduced project level roles. Changed authorization rules for project related screens to support admin functions from project level admins that are not system level admins.
    
    Added support for changing project level roles for Users.
    
    Added support for changing project level roles for Users (missing files).
    
    Code review changes: removed migration script, added up-to-date schema.rb.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -6,8 +6,4 @@ module JobsHelper
   def job_active?
     @job.active? && (JobExecution.find_by_id(@job.id) || JobExecution.enabled)
   end
-
-  def can_create_job?
-    current_user.is_super_admin? || current_user.is_admin?
-  end
 end
